558a41ac9ecd07920215576a9f20cf05ad0faf96
Hu6Thi3n <93976711+Hu6Thi3n@users.noreply.github.com>
2024-09-20 05:32:42 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -41,15 +41,15 @@ end
 local function sendMessageToServer(data)
     local Url = 'http://103.176.24.132:2008/sendMessage'
     
-    local jsonData = HttpService:JSONEncode(data)  -- Convert Lua table to JSON
+    local jsonData = HttpService:JSONEncode(data)
 
     local Response = requests {
         Method = 'POST',
         Url = Url,
         Headers = {
-            ['Content-Type'] = 'application/json',  -- Set the content type to JSON
+            ['Content-Type'] = 'application/json',
         },
-        Body = jsonData,  -- Send the JSON data
+        Body = jsonData,
     }
 
     print("Server Response Status: " .. Response.StatusCode)
@@ -63,37 +63,18 @@ local function sendMessageToServer(data)
 end
 
 while true do 
-    wait(5)
-    local success, err = pcall(function()
-        getgenv().data.units = getunits()
-        getgenv().data.Story = getinfomap() .. "/6"
-        getgenv().data.Level = inventory.Level
-        getgenv().data.time = getgenv().data.time + 1
-        
-        if getgenv().data.first_gem == nil then
-            getgenv().data.first_gem = inventory.Currencies.Gems
-        else
-            getgenv().data.first_gem = inventory.Currencies.Gems
-        end
-        
-        if getgenv().data.first_gem > getgenv().data.last_gem then
-            getgenv().data.first_gem = getgenv().data.last_gem
-        end
-        
-        getgenv().data.last_gem = inventory.Currencies.Gems
-        
-        getgenv().data.Trait_Crystal = inventory.Items["Trait Crystal"] or 0
-        getgenv().data.risky_dice = inventory.Items["Risky Dice"] or 0
-        
-        if game.PlaceId == 17764698696 then
-            getgenv().data.Status = "Lobby"
-        else
-            getgenv().data.Status = "In Game"
-        end
+    wait(15)
+    local LocalPlayer = game.Players.LocalPlayer
+    local playerData = {
+        Username = LocalPlayer.Name,
+        units = getunits(),
+        Story = getinfomap() .. "/6",
+        Level = inventory.Level,
+        Trait_Crystal = inventory.Items["Trait Crystal"] or 0,
+        risky_dice = inventory.Items["Risky Dice"] or 0,
+        Status = (game.PlaceId == 17764698696) and "Lobby" or "In Game",
+    }
 
-        sendMessageToServer(getgenv().data)  -- Gửi dữ liệu đến máy chủ
-
-        writefile(plr.Name..".json", HttpService:JSONEncode(getgenv().data))
-    end)
-    print(success, err)
+    debugLog("Sending player data to the server.")
+    sendMessageToServer(playerData)  -- Gửi dữ liệu người chơi đến máy chủ
 end
